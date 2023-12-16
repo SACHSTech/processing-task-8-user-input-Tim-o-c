5e18fe3050692dd82d0eb3e29e7eef1e41756a31
Tim_o_c <timothy.chhor24@ycdsbk12.ca>
2023-12-16 18:14:29 -0500

    many inputs have been added

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,150 @@
 import processing.core.PApplet;
+import processing.event.MouseEvent;
 
 public class Sketch extends PApplet {
 	
 	
   /**
-   * Called once at the beginning of execution, put your size all in this method
+   * A showcase of the many inputs one can use in coding
+   * @author: T. Chhor
    */
+
   public void settings() {
-	// put your size call here
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    // setting up the background
+    background(20, 20, 20);
+    fill(124, 252, 0);
+    rect(0, 350, 400, 400);
+    textSize(10);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+  // defining variables
+  int intCircleX = 200;
+  int intCircleY = 350;
+  boolean aPressed = false;
+  boolean wPressed = false;
+  boolean sPressed = false;
+  boolean dPressed = false;
+  int rColour = 20;
+  int bColour = 20;
+  int gColour = 20;
+  String specialMessage = "";
+  boolean isTyping = false;
+  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  public void draw() {
+    text("Press 1 for type mode, 2 for move mode", 20, 20);
+
+    // Drawing or typing?
+    if (keyPressed) {
+      if (key == '1') {
+        isTyping = true;
+      }
+      else if (key == '2') {
+        isTyping = false;
+      }
+    }
+
+    // Draws an ellipse wherever the mouse is clicked
+	  if (mousePressed && key != '1' && key != '2' && !isTyping) {
+      clearScreen();
+      ellipse(mouseX, mouseY, 50, 50);
+    }
+    
+    // Control a ball with WASD
+    if (aPressed && !isTyping) {
+      intCircleX--;
+    }
+    if (dPressed && !isTyping) {
+      intCircleX++;
+    }
+    if (sPressed && !isTyping) {
+      intCircleY++;
+    }
+    if (wPressed && !isTyping) {
+      intCircleY--;
+    }
+    if (keyPressed && key != '1' && key != '2' && !isTyping) {
+      if (key == 'a' || key == 'A') {
+        aPressed = true;
+      }
+      if (key == 'w' || key == 'W') {
+        wPressed = true;
+      }
+      if (key == 'd' || key == 'D') {
+        dPressed = true;
+      }
+      if (key == 's' || key == 'S') {
+        sPressed = true;
+      }
+      clearScreen();
+    }
+    ellipse(intCircleX, intCircleY, 50, 50);
+    text(specialMessage, 20, 40);
   }
   
-  // define other methods down here.
+  /**
+   * Clears the screen
+   */
+  private void clearScreen() {
+        background(rColour, gColour, bColour);
+        fill(124, 252, 0);
+        rect(0, 350, 400, 400);
+  }
+  
+  /** 
+   * Sets important values to false when keys are not pressed
+  */
+  public void keyReleased() {
+      if (key != '1' && key != '2' && !isTyping){
+        if (key == 'a' || key == 'A') {
+          aPressed = false;
+        }
+        if (key == 'w' || key == 'W') {
+          wPressed = false;
+        }
+        if (key == 'd' || key == 'D') {
+          dPressed = false;
+        }
+        if (key == 's' || key == 'S') {
+          sPressed = false;
+        }
+      }
+    }
+
+  /**
+   * While holding r, g or b, you can change the rgb values by scrolling with the mouse wheel 
+   */
+  public void mouseWheel(MouseEvent event) {
+    if (keyPressed && key != '1' && key != '2' && !isTyping) {
+      keyPressed();
+      if (key == 'r' || key == 'R') {
+        rColour += 20 * event.getCount();
+        if (rColour > 255 || rColour < 0) {
+          rColour = 20;
+        }
+      }
+      else if (key == 'g' || key == 'G') {
+        gColour += 20 * event.getCount();
+        if (gColour > 255 || gColour < 0) {
+          gColour = 20;
+      }
+      }
+      else if (key == 'b' || key == 'B') {
+        bColour += 20 * event.getCount();
+        if (bColour > 255 || bColour < 0) {
+          bColour = 20;
+        }
+      }
+    }
+  }
+
+  public void keyTyped() {
+    if (keyPressed && isTyping && key != '1' && key != '2') {
+      specialMessage += key; 
+    }
+  }
 }
